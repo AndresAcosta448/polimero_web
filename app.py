995dad00edc564f53fd119f37149ebaaee2be774
Andres Felipe Acosta Cifuentes <pipecifuentes204@gmail.com>
2025-06-24 00:32:05 -0500

    Descripción de tus cambios

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ def get_db_connection():
     try:
         conn = mysql.connector.connect(
             host     = os.getenv("DB_HOST"),
-            port     = int(os.getenv("DB_PORT", 3306)),
+            port     = int(os.getenv("DB_PORT", 28635)),
             user     = os.getenv("DB_USER"),
             password = os.getenv("DB_PASSWORD"),
             database = os.getenv("DB_NAME")
